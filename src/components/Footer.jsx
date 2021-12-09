decb8240b79e0c2f0970d8259decf9240e854132
Advaith Chevuturu <advaith.c@gmail.com>
2021-12-09 14:23:25 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { repoLink } from "../user/user";
+
+const Footer = () => {
+  return (
+    <div className="container footer-container">
+      <ul>
+        <li title="GitHub Repo.">
+          <a href={repoLink}>
+            Project Repo. <i className="fab fa-github"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
