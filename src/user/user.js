65ae16583810e6f702e21d21ea3dd71d788e6b9f
Advaith Chevuturu <advaith.c@gmail.com>
2021-12-09 14:22:30 +0530

    Add files via upload

---
+++ src/user/user.js
@@ -0,0 +1,71 @@
+const userName = "Advaith Aditya Chevuturu";
+const githubLink = "https://github.com/AdvaithAdityaChevuturu";
+// const twitterLink = "";
+const linkedinLink = "https://www.linkedin.com/in/advaith-aditya-chevuturu/";
+const repoLink = "https://github.com/AdvaithAdityaChevuturu/Portfolio";
+const fiverrLink = "";
+
+const mainDesc = "Project Developer";
+const aboutDesc = `Hey! I'm ${userName}, A third year student of Information Technology at VIT Vellore. I worked on a few projects, from websites using React, Node and PHP to research level projects including cryptography algorithms and data mining. I prefer coding in Python and C++. In my spare time, I play table tennis and video games.`;
+
+const langs = [
+  {
+    name: "Data Structures",
+    percentage: 65,
+    color: "#581845"
+  },
+  {
+    name: "Software Engineering Principles",
+    percentage: 80,
+    color: "#f7df1e"
+  },
+  {
+    name: "Interpersonal Communication",
+    percentage: 80,
+    color: "#61dafb"
+  },
+  {
+    name: "Python",
+    percentage: 75,
+    color: "#3476ab"
+  },
+  {
+    name: "C/C++",
+    percentage: 70,
+    color: "#649bd3"
+  },
+  {
+    name: "HTML & CSS",
+    percentage: 65,
+    color: "#e85324"
+  }
+];
+
+const projects = [
+  {
+    title: "CareIND",
+    desc:
+      "A location based donation platform to help the people in need, hosted using XAMMP (PHP,HTML,CSS,MYSQL)",
+    demo: "",
+    repo: "https://github.com/AdvaithAdityaChevuturu/CareIND"
+  },
+  {
+    title: "Videogame Retail Database",
+    desc: "DBMS project which aims to capture a sample database system used in a game distribution system similar to steam.",
+    demo: "",
+    repo: "https://github.com/AdvaithAdityaChevuturu/Video-game-retail-database"
+  }
+];
+
+export {
+  userName,
+  githubLink,
+  // twitterLink,
+  linkedinLink,
+  repoLink,
+  fiverrLink,
+  mainDesc,
+  aboutDesc,
+  langs,
+  projects
+};
